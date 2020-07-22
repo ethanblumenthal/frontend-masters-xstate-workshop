bea5a9272ea9a35e7bc9af5b81c237bffa571d16
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-07-22 14:44:38 -0500

    Completed sections 5 and 6.

--- src/06/index.js
+++ src/06/index.js
@@ -1,6 +1,6 @@
-import { createMachine, assign, interpret } from 'xstate';
+import { createMachine, assign, interpret } from "xstate";
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 const elBody = document.body;
 
 const assignPoint = assign({
@@ -38,7 +38,7 @@ const resetPosition = assign({
 });
 
 const machine = createMachine({
-  initial: 'idle',
+  initial: "idle",
   context: {
     x: 0,
     y: 0,
@@ -54,26 +54,32 @@ const machine = createMachine({
         mousedown: {
           // Don't select this transition unless
           // there are < 5 drags
-          // ...
+          cond: (context, event) => {
+            return context.drags < 5;
+          },
           actions: assignPoint,
-          target: 'dragging',
+          target: "dragging",
         },
       },
     },
     dragging: {
       // Whenever we enter this state, we want to
       // increment the drags count.
-      // ...
+      entry: assign({
+        drags: (context) => {
+          return context.drags + 1;
+        },
+      }),
       on: {
         mousemove: {
           actions: assignDelta,
         },
         mouseup: {
           actions: [assignPosition],
-          target: 'idle',
+          target: "idle",
         },
-        'keyup.escape': {
-          target: 'idle',
+        "keyup.escape": {
+          target: "idle",
           actions: resetPosition,
         },
       },
@@ -90,29 +96,29 @@ service.onTransition((state) => {
     elBox.dataset.state = state.value;
     elBox.dataset.drags = state.context.drags;
 
-    elBox.style.setProperty('--dx', state.context.dx);
-    elBox.style.setProperty('--dy', state.context.dy);
-    elBox.style.setProperty('--x', state.context.x);
-    elBox.style.setProperty('--y', state.context.y);
+    elBox.style.setProperty("--dx", state.context.dx);
+    elBox.style.setProperty("--dy", state.context.dy);
+    elBox.style.setProperty("--x", state.context.x);
+    elBox.style.setProperty("--y", state.context.y);
   }
 });
 
 service.start();
 
-elBox.addEventListener('mousedown', (event) => {
+elBox.addEventListener("mousedown", (event) => {
   service.send(event);
 });
 
-elBody.addEventListener('mousemove', (event) => {
+elBody.addEventListener("mousemove", (event) => {
   service.send(event);
 });
 
-elBody.addEventListener('mouseup', (event) => {
+elBody.addEventListener("mouseup", (event) => {
   service.send(event);
 });
 
-elBody.addEventListener('keyup', (e) => {
-  if (e.key === 'Escape') {
-    service.send('keyup.escape');
+elBody.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    service.send("keyup.escape");
   }
 });
