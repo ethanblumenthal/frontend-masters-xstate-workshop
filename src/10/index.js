bb1a45decf3151e7f21cfb2b5416b4d1c5d27c1e
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-07-27 10:24:27 -0500

    Completed nested states and history states exercises.

--- src/10/index.js
+++ src/10/index.js
@@ -1,26 +1,37 @@
-import { createMachine, interpret } from 'xstate';
+import { createMachine, interpret } from "xstate";
 
-const elApp = document.querySelector('#app');
-const elOffButton = document.querySelector('#offButton');
-const elOnButton = document.querySelector('#onButton');
-const elModeButton = document.querySelector('#modeButton');
+const elApp = document.querySelector("#app");
+const elOffButton = document.querySelector("#offButton");
+const elOnButton = document.querySelector("#onButton");
+const elModeButton = document.querySelector("#modeButton");
 
 const displayMachine = createMachine({
-  initial: 'hidden',
+  initial: "hidden",
   states: {
     hidden: {
       on: {
-        TURN_ON: 'visible',
+        TURN_ON: "visible.hist",
       },
     },
     visible: {
-      // Add hierarchical states for light/dark mode.
-      // ...
-
-      // Then, add a history state that remembers which mode we used.
-      // ...
+      initial: "light",
+      states: {
+        light: {
+          on: {
+            SWITCH: "dark",
+          },
+        },
+        dark: {
+          on: {
+            SWITCH: "light",
+          },
+        },
+        hist: {
+          type: "history",
+        },
+      },
       on: {
-        TURN_OFF: 'hidden',
+        TURN_OFF: "hidden",
       },
     },
   },
@@ -28,23 +39,18 @@ const displayMachine = createMachine({
 
 const displayService = interpret(displayMachine)
   .onTransition((state) => {
-    elApp.dataset.state = state.toStrings().join(' ');
+    elApp.dataset.state = state.toStrings().join(" ");
   })
   .start();
 
-// Add event listeners for:
-// - clicking elOnButton (TURN_ON)
-// - clicking elOffButton (TURN_OFF)
-// - clicking elModeButton (SWITCH)
-
-elOnButton.addEventListener('click', () => {
-  displayService.send('TURN_ON');
+elOnButton.addEventListener("click", () => {
+  displayService.send("TURN_ON");
 });
 
-elOffButton.addEventListener('click', () => {
-  displayService.send('TURN_OFF');
+elOffButton.addEventListener("click", () => {
+  displayService.send("TURN_OFF");
 });
 
-elModeButton.addEventListener('click', () => {
-  displayService.send('SWITCH');
+elModeButton.addEventListener("click", () => {
+  displayService.send("SWITCH");
 });
